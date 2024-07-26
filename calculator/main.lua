d473e87de16cf850bd5245d0cc66f80a243eb8b2
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-07-26 10:52:39 +0300

    26/07/2024

---
+++ calculator/main.lua
@@ -0,0 +1,31 @@
+require "import"
+import "android.widget.*"
+import "android.view.*"
+layout={
+    LinearLayout;
+    {
+        EditText;
+        hint="type hear";
+        id="code"
+    };
+    {
+        Button;
+        text="calculate";
+        onClick=function()
+            local result,error=loadstring("return " .. code.getText().toString())
+            if result then
+                local success,output=pcall(result)
+                resultText.setText(tostring(output))
+            else
+                resultText.setText(tostring(error))
+            end
+        end;
+    };
+    {
+        TextView;
+        id="resultText";
+    }
+}
+dlg=activity
+dlg.setTitle("calculator")
+dlg.setContentView(loadlayout(layout))
